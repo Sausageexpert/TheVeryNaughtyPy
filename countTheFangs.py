b6b92c75a66ed074cc619fe115311456e0d462b2
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2021-08-15 16:38:18 +0530

    Update countTheFangs.py
    Py aka Wiggly aka Slippie is back to woo us with his skills ;-)

--- countTheFangs.py
+++ countTheFangs.py
@@ -2,19 +2,21 @@ import random
 rand = random.randint(1, 9)
 chances = 5
 
-guess = int(input("How Many Supporters Does Mr. Py Have In The Parliament! (In Thousands) "))
+
 
 while chances > 0:
+   guess = int(input("How Many Supporters Does Mr. Py Have In The Parliament! (In Thousands) "))
    if guess < rand:
       print("Come on! Mr. Py's pretty popular!")
-      break
-   if guess == rand:
+      
+   elif guess == rand:
       print("Yep, that's correct!")
       break
-   if guess > rand:
-      print("Woah, Parliament doesn't even have that many people!")
-      break
-   
-chances = chances - 1
-   
+      
+   else:
+      print("Woah, Parliament doesn't even have that many people!Try again")
+   chances = chances - 1
+      
    #Please work?
+   
+    
